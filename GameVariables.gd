ae27e328e9acfd9ea72039094d4d6c5a7fec49b9
gweltou <gweltou@hotmail.com>
2021-11-19 20:20:10 +0100

    collected letters bigger display size

--- GameVariables.gd
+++ GameVariables.gd
@@ -1,17 +1,15 @@
 extends Node
 
 
-const bullet_font_size = 16
-
-
 var bullet_font = DynamicFont.new()
-
-
+const bullet_font_size = 14
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	bullet_font.font_data = load("res://assets/Fipps-Regular.otf")
 	bullet_font.size = bullet_font_size
+	bullet_font.extra_spacing_char = 2
+	bullet_font.extra_spacing_bottom = -12
 	pass # Replace with function body.
 
 
