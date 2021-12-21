a7cedfc797827acc6df75ee82014292cb24bdbe5
gweltou <gweltou@hotmail.com>
2021-12-21 21:32:08 +0100

    pause button 'p'

--- seu/Bullet.gd
+++ seu/Bullet.gd
@@ -22,7 +22,7 @@ signal letter_collected(letter)
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	pause_mode = Node.PAUSE_MODE_STOP
 
 
 func _physics_process(delta):
