428cd635f4051b719a8bc3487892672a1de55006
gweltou <gweltou@hotmail.com>
2022-01-26 15:28:01 +0100

    eyelids closing

--- title/TitleScreen.gd
+++ title/TitleScreen.gd
@@ -13,10 +13,14 @@ var pawHeight : float
 
 var menu_idx := 0
 onready var menu_pos := [btnStoryPos, btnArcadePos, btnOptionPos, btnQuitPos]
+var eyelid_counter = 0.0
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	$Cat/Eyelid1.visible = false
+	$Cat/Eyelid2.visible = false
+	
 	pawHeight = Paw.get_sprite_frames().get_frame("default", 0).get_size().y
 	pawHeight *= Paw.scale.y
 	Paw.position.y = menu_pos[menu_idx]
@@ -39,9 +43,17 @@ func _unhandled_input(event):
 		$MenuAccept.play()
 	
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _process(delta):
+	if eyelid_counter <= 0.0:
+		$Cat/Eyelid1.visible = false
+		$Cat/Eyelid2.visible = false
+		if randf() < 0.005:
+			$Cat/Eyelid1.visible = true
+			$Cat/Eyelid2.visible = true
+			eyelid_counter = 0.2
+	else:
+		eyelid_counter -= delta
+		
 
 func _on_Paw_animation_finished():
 	Paw.stop()
