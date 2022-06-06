5df5163f850afbca91da5b6ef78f22c0557ff4a1
gweltou <gweltou@hotmail.com>
2022-06-06 23:06:54 +0200

    🎮 vibrates when hit

--- seu/Player.gd
+++ seu/Player.gd
@@ -43,6 +43,7 @@ func hit(bullet : Bullet):
 		var dir = position - bullet.position
 		apply_central_impulse(dir * 80)
 		apply_torque_impulse(1000 * (randf() - 0.5))
+		Input.start_joy_vibration(0, 0, 1, 0.1)
 	else:
 		if $PlayerLetter.is_playing():
 			$PlayerLetter2.play()
